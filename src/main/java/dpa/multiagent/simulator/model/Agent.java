7b45fc911798077526e4fdb5f9ccd0cda9e8c9d4
owen <o.w.varley@gmail.com>
2018-12-06 12:02:21 +0000

    Adding Get/Set for name

--- src/main/java/dpa/multiagent/simulator/model/Agent.java
+++ src/main/java/dpa/multiagent/simulator/model/Agent.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Agent {
+
     private String name;
     private List<Point> agents;
 
@@ -13,4 +14,13 @@ public class Agent {
         name = "";
         agents = new ArrayList<>();
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
