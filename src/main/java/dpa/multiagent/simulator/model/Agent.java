6339775d8420bb8f6aa0f4bb5f17c9e87b7e62a4
owen <o.w.varley@gmail.com>
2018-12-06 11:59:38 +0000

    Added agent class Addd model package

---
+++ src/main/java/dpa/multiagent/simulator/model/Agent.java
@@ -0,0 +1,16 @@
+package dpa.multiagent.simulator.model;
+
+import java.awt.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Agent {
+    private String name;
+    private List<Point> agents;
+
+    public Agent()
+    {
+        name = "";
+        agents = new ArrayList<>();
+    }
+}
