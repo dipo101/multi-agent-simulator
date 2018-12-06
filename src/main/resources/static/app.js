466aa78db8766d9b01744f32c3f9d4786a13ac14
Xiao Liu <liux0047@gmail.com>
2018-12-06 14:41:48 +0000

    socket working

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -9,7 +9,6 @@ function connect() {
     var socket = new SockJS('/gs-guide-websocket');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
-        setConnected(true);
         console.log('Connected: ' + frame);
         stompClient.subscribe('/topic/greetings', function (data) {
             updateGraph(JSON.parse(data.body).content);
@@ -18,7 +17,7 @@ function connect() {
 }
 
 function sendSetting() {
-    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+    stompClient.send("/app/hello", {}, JSON.stringify({'a1': $("#a1").val()}));
 }
 
 function disconnect() {
