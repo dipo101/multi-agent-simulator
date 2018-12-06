8390a83425574e8663477e372de43b5d3291eac8
owen <o.w.varley@gmail.com>
2018-12-06 14:42:10 +0000

    Expanding Agent functionality Adding framework for collision checking

--- src/main/java/dpa/multiagent/simulator/model/Agent.java
+++ src/main/java/dpa/multiagent/simulator/model/Agent.java
@@ -1,18 +1,43 @@
 package dpa.multiagent.simulator.model;
 
 import java.awt.*;
+import java.awt.geom.Point2D;
 import java.util.List;
 import java.util.ArrayList;
 
 public class Agent {
 
     private String name;
-    private List<Point> path;
+    private List<Point2D> path;
 
-    public Agent()
-    {
+    public void updatePos(Point2D pt) {
+        path.add(pt);
+    }
+
+    public Point2D getLatestPos() throws Exception {
+        if (path.size() > 1) {
+            return path.get(path.size() - 1);
+        } else {
+            throw new Exception();
+        }
+    }
+
+    public Point2D getNextPos(double angle, int speed) {
+        Point2D pt = new Point2D.Double();
+        double x, y;
+
+        x = Math.sin(angle);
+        y = Math.cos(angle);
+
+        pt.setLocation(x, y);
+
+        return pt;
+    }
+
+    public Agent() {
         name = "";
         path = new ArrayList<>();
+        path.add(new Point2D.Double(0, 0));
     }
 
     public String getName() {
--- src/main/java/dpa/multiagent/simulator/model/Simulator.java
+++ src/main/java/dpa/multiagent/simulator/model/Simulator.java
@@ -1,10 +1,63 @@
 package dpa.multiagent.simulator.model;
 
+import java.awt.geom.Point2D;
 import java.util.List;
 
 public class Simulator {
     List<Agent> agents;
     SimulatorSettings settings;
+
+    public boolean checkCollision(Agent caller, Point2D currPos, Point2D nextPos) throws Exception {
+        Point2D currPosAgent2, nextPosAgent2;
+
+        for (Agent a: agents)
+        {
+            if (a.equals(caller))
+                continue;
+
+            currPosAgent2 = a.getLatestPos();
+            nextPosAgent2 = a.getNextPos(getAngle(), getSpeed());
+
+            // Compare currPos currPosAgent2, etc to determine collision
+        }
+
+        return false;
+    }
+
+    public void update() throws Exception {
+        Point2D pt;
+
+        for (Agent a : agents) {
+            pt = a.getNextPos(getAngle(), getSpeed());
+            if(checkCollision(a, a.getLatestPos(), pt)) {
+                a.updatePos(pt);
+            }
+        }
+    }
+
+    public double getAngle() {
+        double ret = 0;
+
+        switch (settings.getAngleSettings().angleSamplingStrategy) {
+            case LINEAR:
+                ret = (settings.minAnglePerFrame + settings.maxAnglePerFrame) / 2;
+                break;
+        }
+
+        return ret;
+    }
+
+    public int getSpeed() {
+        int ret = 0;
+
+        switch (settings.getSpeedSettings().speedSamplingStrategy) {
+            case LINEAR:
+                ret = (settings.minSpeedPerFrame + settings.maxSpeedPerFrame) / 2;
+                break;
+        }
+
+        return ret;
+    }
 
     public List<Agent> getAgents() {
         return agents;
