767c331e3a3d1c20c04b9eff620af37c997c9a42
owen <o.w.varley@gmail.com>
2018-12-06 11:02:12 +0000

    Initial commit

---
+++ src/main/java/dpa/multiagent/simulator/MultiAgentSimulatorApplication.java
@@ -0,0 +1,12 @@
+package dpa.multiagent.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiAgentSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiAgentSimulatorApplication.class, args);
+	}
+}
